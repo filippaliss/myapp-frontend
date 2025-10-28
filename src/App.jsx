e0c17a99d213e3d5a621331e417c07a66c77545e
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-10-28 23:22:03 +0100

    making gh-pages worke white the mailing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Doc from "./pages/Doc";
 import Layout from "./components/Layout";
@@ -7,7 +7,7 @@ import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/myapp-frontend">
+    <HashRouter basename="/myapp-frontend">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,6 +17,6 @@ export default function App() {
           <Route path="/doc/:id" element={<Doc />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
